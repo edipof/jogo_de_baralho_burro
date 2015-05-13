ecd039bf4859f58a03b537ebc11f0738c30adc96
edipof <edipofernandes7@gmail.com>
2015-05-13 10:55:50 -0300

    as

--- src/Jogador/JogadorBaralho.java
+++ src/Jogador/JogadorBaralho.java
@@ -1,5 +1,13 @@
 package Jogador;
 
-public interface JogadorBaralho {
+import java.util.ArrayList;
 
+import baralho.Carta;
+
+public interface JogadorBaralho {
+	
+	public ArrayList<Carta> mao = new ArrayList<Carta>();
+	
+	public ArrayList<Carta> getMao(ArrayList<Carta> cartas);
+	public void setMao(ArrayList<Carta> cartas);
 }
--- src/Jogador/JogadorHumano.java
+++ src/Jogador/JogadorHumano.java
@@ -1,5 +1,21 @@
 package Jogador;
 
+import java.util.ArrayList;
+
+import baralho.Carta;
+
 public class JogadorHumano extends Pessoa implements JogadorBaralho {
+
+	@Override
+	public ArrayList<Carta> getMao(ArrayList<Carta> cartas) {
+		
+		return mao;
+	}
+
+	@Override
+	public void setMao(ArrayList<Carta> cartas) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
